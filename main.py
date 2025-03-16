6a0d17eabb896f49859646467dacea9538b3d9fa
Susan Hatem <susanhat@umich.edu>
2025-03-16 22:13:04 +0000

    parsing arguments and testing with iris.csv

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import pandas as pd
 import numpy as np
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
+from sklearn.datasets import load_breast_cancer
+import argparse
 
 
-def load_data(path: str, columns: List[str]) -> pd.DataFrame:
+def load_data(path: str, columns: list[str]) -> pd.DataFrame:
     """
     Load data from passed in CSV path and select columns for k-means clustering
 
-    input: str, List[str]
+    input: str, list[str]
 
     output: pd.DataFrame
     """
@@ -37,13 +39,26 @@ def create_kmeans_clusters(data: np.ndarray, n_clusters: int) -> np.ndarray:
     output: np.ndarray
     """
     k = KMeans(n_clusters=n_clusters, random_state=42)
-    classification = k.fit_predict(data)
-    return classification
+    labels = k.fit_predict(data)
+    return labels
 
 
 def parse_args():
+    parser = argparse.ArgumentParser(description="K-means clustering script")
+    parser.add_argument('path', help="CSV file path")
+    parser.add_argument('columns', help="Columns to use for clustering", nargs='+')
+    parser.add_argument('clusters', help="Number of clusters", type=int)
+
+    return parser.parse_args()
 
 if __name__ == "__main__":
+    args = parse_args()
+    df = load_data(args.path, args.columns)
+    scaled = scale_data(df)
+    labels = create_kmeans_clusters(scaled, args.clusters)
+
+    print("Labels: ")
+    print(labels)
 
 
 
