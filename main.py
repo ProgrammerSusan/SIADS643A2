09ed0dd1530397127a59c0b84a2374c618dde3dd
Susan Hatem <susanhat@umich.edu>
2025-03-16 21:47:33 +0000

    Inital creation of k-means clustering generic logic

--- main.py
+++ main.py
@@ -1,8 +1,45 @@
-def load_data():
+import pandas as pd
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
 
-def scale_data():
 
-def create_kmeans_clusters():
+def load_data(path: str, columns: List[str]) -> pd.DataFrame:
+    """
+    Load data from passed in CSV path and select columns for k-means clustering
+
+    input: str, List[str]
+
+    output: pd.DataFrame
+    """
+    df = pd.read_csv(path)
+    return df[columns]
+
+
+def scale_data(df: pd.DataFrame):
+    """
+    Scale data for k-means clustering
+
+    input: pd.DataFrame
+
+    output: np.ndarray
+    """
+    scaler = StandardScaler()
+    return scaler.fit_transform(df)
+
+
+def create_kmeans_clusters(data: np.ndarray, n_clusters: int) -> np.ndarray:
+    """
+    Apply k-means clustering to data
+
+    input: pd.DataFrame, int
+
+    output: np.ndarray
+    """
+    k = KMeans(n_clusters=n_clusters, random_state=42)
+    classification = k.fit_predict(data)
+    return classification
+
 
 def parse_args():
 
